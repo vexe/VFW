96ed2e2b6d967288f6957daf746ef4fa365d9f19
vexe <xarxoohx@gmail.com>
2015-04-28 23:01:41 -0700

    Introduced Vexe.Editor.Extensions with some handy 'Event' extensions

---
+++ Assets/Plugins/Editor/Vexe/EditorExtensions.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Vexe.Editor.Extensions
+{
+    public static class EditorExtensions
+    {
+        /// <summary>
+        /// Returns true if this rect contains the current mouse position
+        /// </summary>
+        public static bool ContainsMouse(this Rect rect)
+        {
+            return rect.Contains(Event.current.mousePosition);
+        }
+
+        public static bool IsLMB(this Event e)
+        {
+            return e.button == 0;
+        }
+
+        public static bool IsRMB(this Event e)
+        {
+            return e.button == 1;
+        }
+
+        public static bool IsMMB(this Event e)
+        {
+            return e.button == 2;
+        }
+
+        public static bool IsMouseDown(this Event e)
+        {
+            return e.type == EventType.MouseDown;
+        }
+
+        public static bool IsDoubleClick(this Event e)
+        {
+            return e.clickCount == 2;
+        }
+
+        public static bool IsLMBDown(this Event e)
+        {
+            return IsMouseDown(e) && IsLMB(e);
+        }
+
+        public static bool IsRMBDown(this Event e)
+        {
+            return IsMouseDown(e) && IsRMB(e);
+        }
+
+        public static bool IsMMBDown(this Event e)
+        {
+            return IsMouseDown(e) && IsMMB(e);
+        }
+    }
+}
