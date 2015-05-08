6b4ce9a956a945ff6ca939af85e9c43ce03b28bc
vexe <xarxoohx@gmail.com>
2015-05-08 14:42:51 -0700

    Removed the TD generic argument from DictionaryDrawer simplifying it

--- Assets/Plugins/Editor/Vexe/Drawers/API/TypeDrawerMapper.cs
+++ Assets/Plugins/Editor/Vexe/Drawers/API/TypeDrawerMapper.cs
@@ -84,20 +84,23 @@ namespace Vexe.Editor
             {
                 this.Add(typeof(List<>), typeof(ListDrawer<>), true, BakeTypeArgsInDrawer)
                     .Add(typeof(Array), typeof(ArrayDrawer<>), true, (a, d) => d.MakeGenericType(a.GetElementType()))
-                    .Add(typeof(IDictionary<,>), typeof(IDictionaryDrawer<,,>), true, (a, d) =>
+                    .Add(typeof(IDictionary<,>), typeof(IDictionaryDrawer<,>), true, (a, d) =>
                     {
+                        if (a.IsAbstract)
+                        {
+                            Debug.LogError("Mapping error: IDictionary type {0} is abstract thus the drawer can't instantiate an instance of it. " +
+                                           "Falling back to RecursiveDrawer".FormatWith(a));
+                            return typeof(RecursiveDrawer);
+                        }
+
                         if (a.GetConstructor(Type.EmptyTypes) == null)
                         {
                             Debug.LogError("Mapping error: IDictionary type {0} must have a public empty constructor in order for it to be mapped with {1}. " +
-                                "Falling back to RecursiveDrawer".FormatWith(a, d));
+                                           "Falling back to RecursiveDrawer".FormatWith(a, d));
                             return typeof(RecursiveDrawer);
                         }
-                        var typeArgs = a.GetGenericArgsInThisOrAbove();
-                        var drawerArgs = new Type[typeArgs.Length + 1];
-                        drawerArgs[0] = a;
-                        for (int i = 0; i < typeArgs.Length; i++)
-                            drawerArgs[i + 1] = typeArgs[i];
-                        return d.MakeGenericType(drawerArgs);
+
+                        return BakeTypeArgsInDrawer(a, d);
                     });
             }
             // Nullable
@@ -237,7 +240,7 @@ namespace Vexe.Editor
         /// </summary>
         public Type BakeTypeArgsInDrawer(Type type, Type drawerType)
         {
-            return drawerType.MakeGenericType(type.GetGenericArguments());
+            return drawerType.MakeGenericType(type.GetGenericArgsInThisOrAbove());
         }
     }
 }
