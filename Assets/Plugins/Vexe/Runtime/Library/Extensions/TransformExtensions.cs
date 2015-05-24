296cbe370292092eaeb92127a354987162a14d2f
vexe <xarxoohx@gmail.com>
2015-05-24 00:57:30 -0700

    Added Activate/Deactivate Transform extension methods

--- Assets/Plugins/Vexe/Runtime/Library/Extensions/TransformExtensions.cs
+++ Assets/Plugins/Vexe/Runtime/Library/Extensions/TransformExtensions.cs
@@ -4,6 +4,16 @@ namespace Vexe.Runtime.Extensions
 {
     public static class TransformExtensions
     {
+        public static void Activate(this Transform t)
+        {
+            t.gameObject.Activate();
+        }
+
+        public static void Deactivate(this Transform t)
+        {
+            t.gameObject.Deactivate();
+        }
+
         /// <summary>
         /// Sets localPosition to Vector3.zero, localRotation to Quaternion.identity, and localScale to Vector3.one
         /// </summary>
