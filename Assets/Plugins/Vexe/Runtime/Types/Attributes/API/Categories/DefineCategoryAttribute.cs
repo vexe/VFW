70c25b0150d20b67c78228535e24127d852df91e
vexe <xarxoohx@gmail.com>
2015-07-01 00:50:40 -0700

    Cleanup/Refactoring take 1
    - Moved the category files from the Runtime folder to Attributes/API

--- Assets/Plugins/Vexe/Runtime/Types/Attributes/API/Categories/DefineCategoryAttribute.cs
+++ Assets/Plugins/Vexe/Runtime/Types/Attributes/API/Categories/DefineCategoryAttribute.cs
@@ -12,60 +12,60 @@ namespace Vexe.Runtime.Types
 		/// Allows you to include members by their MemberType i.e. Field, Property, Method or All
 		/// You can 'or' (add) multiple member types together ex MemberType.Field | MemberType.Property
 		/// </summary>
-		public CategoryMemberType MemberType { get; set; }
+		public CategoryMemberType MemberType;
 
 		/// <summary>
 		/// Allows you to include members whose name matches this regex pattern
 		/// </summary>
-		public string Pattern { get; set; }
+		public string Pattern;
 
 		/// <summary>
 		/// Allows you to include members whose data type matches this type (ex methods that return int etc)
 		/// </summary>
-		public Type DataType { get; set; }
+		public Type DataType;
 
 		/// <summary>
 		/// The full path of this defintion split by '/'
 		/// </summary>
-		public string FullPath { get; private set; }
+		public readonly string FullPath;
 
 		/// <summary>
 		/// Allows you to explictly include members by name
 		/// </summary>
-		public string[] ExplicitMembers { get; private set; }
+		public readonly string[] ExplicitMembers;
 
 		/// <summary>
 		/// The descending sorting order of the defined category.
 		/// </summary>
-		public float DisplayOrder { get; set; }
+		public float Order;
 
 		/// <summary>
 		/// If true, the members for this category will not appear in other categories
 		/// NOTE: Categories are sorted when they're processed such that exclusive ones come first
 		/// Default: true
 		/// </summary>
-		public bool Exclusive { get; set; }
+		public bool Exclusive;
 
 		/// <summary>
 		/// Whether to perform a union or intersection on the definition rules
 		/// Default: SetOp.Intersect
 		/// </summary>
-		public CategorySetOp Grouping { get; set; }
+		public CategorySetOp Grouping;
 
 		/// <summary>
 		/// If true, the category will always be expanded
 		/// </summary>
-		public bool ForceExpand { get; set; }
+		public bool ForceExpand;
 
 		/// <summary>
 		/// Category header shown?
 		/// </summary>
-		public bool AlwaysHideHeader { get; set; }
+		public bool AlwaysHideHeader;
 
 		public DefineCategoryAttribute(string fullPath, float displayOrder, params string[] explicitMembers)
 		{
 			FullPath = fullPath;
-			DisplayOrder = displayOrder;
+			Order = displayOrder;
 			ExplicitMembers = explicitMembers;
 			Grouping = CategorySetOp.Intersection;
 			Exclusive = true;
