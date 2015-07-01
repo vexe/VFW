a026060cce5f7a0fd407f2b9e9d5dab84331e721
vexe <xarxoohx@gmail.com>
2015-07-01 01:00:21 -0700

    Cleanup/Refactoring take 3
    - BetterAnimator is no more, the stuff that was in it are now extension methods for Animator

---
+++ Assets/Plugins/Vexe/Runtime/Library/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Vexe.Runtime.Extensions
+{
+	public static class AnimatorExtensions
+	{
+        public static void Bools(this Animator animator, string name, bool value) { animator.SetBool(name, value); }
+        public static bool Bools(this Animator animator, string name) { return animator.GetBool(name); }
+
+        public static void Floats(this Animator animator, string name, float value) { animator.SetFloat(name, value); }
+        public static float Floats(this Animator animator, string name) { return animator.GetFloat(name); }
+
+        public static void Ints(this Animator animator, string name, int value) { animator.SetInteger(name, value); }
+        public static int Ints(this Animator animator, string name) { return animator.GetInteger(name); }
+
+		public static bool IsInState(this Animator animator, int layerIndex, string stateName) { return animator.GetCurrentAnimatorStateInfo(layerIndex).IsName(stateName); }
+		public static bool IsInState(this Animator animator, int layerIndex, int stateHash) { return animator.GetCurrentAnimatorStateInfo(layerIndex).fullPathHash == stateHash; }
+		public static bool IsInState(this Animator animator, string stateName) { return IsInState(animator, 0, stateName); }
+		public static bool IsInState(this Animator animator, int stateHash) { return IsInState(animator, 0, stateHash); }
+	}
+}
