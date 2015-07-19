10fda03c174178899045004653fd4e93b93a1974
rodobodolfo <peladovii@hotmail.com>
2015-07-19 00:55:42 -0300

    HelpButton Attribute
    Adds a toggleable help button that shows a help box.

---
+++ Assets/Plugins/Editor/Vexe/Drawers/User/Others/HelpButtonDrawer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using Vexe.Runtime.Extensions;
+using Vexe.Runtime.Types;
+
+namespace Vexe.Editor.Drawers
+{
+    public class HelpButtonDrawer : CompositeDrawer<object, HelpButtonAttribute>
+    {
+        private string buttonLabel = "?";
+        private bool showHelp = false;
+
+        public override void OnRightGUI()
+        {
+            if (attribute.ButtonPosition != "Left")
+            {
+               showHelp = gui.ToggleButton(showHelp, buttonLabel, buttonLabel, "Help", MiniButtonStyle.Right);     
+            }
+        }
+
+        public override void OnLeftGUI()
+        {
+            if (attribute.ButtonPosition == "Left")
+            {
+                showHelp = gui.ToggleButton(showHelp, buttonLabel, buttonLabel, "Help", Vexe.Editor.MiniButtonStyle.Right);                        
+            }
+        }
+
+        public override void OnUpperGUI()
+        {
+            if (attribute.HelpPosition == "Upper")
+            {
+                if (showHelp)
+                {
+                    gui.HelpBox(attribute.HelpText);      
+                }
+            }
+        }
+
+        public override void OnLowerGUI()
+        {
+            if (attribute.HelpPosition != "Upper")
+            {
+                if (showHelp)
+                {
+                    gui.HelpBox(attribute.HelpText);
+                }
+            }
+        }
+
+    }
+}
