58bfe0473825d7cc3ac3e94f7f6843702517c03e
rodobodolfo <peladovii@hotmail.com>
2015-07-19 18:48:42 -0300

    Comment attribute: Add button parameter
    Adds a "helpButton" parameter to the Comment attribute. If true, it will
    add a toggle button to show/hide the comment.

--- Assets/Plugins/Editor/Vexe/Drawers/User/Decorates/CommentDrawer.cs
+++ Assets/Plugins/Editor/Vexe/Drawers/User/Decorates/CommentDrawer.cs
@@ -5,9 +5,28 @@ namespace Vexe.Editor.Drawers
 {
 	public class CommentDrawer : CompositeDrawer<object, CommentAttribute>
 	{
+        private string buttonLabel = "?";
+        private bool showHelp;
+
+        public override void OnRightGUI()
+        {
+            if (attribute.helpButton)
+                showHelp = gui.ToggleButton(showHelp, buttonLabel, buttonLabel, "Help", MiniButtonStyle.Right);
+        }
+
 		public override void OnUpperGUI()
 		{
-			gui.HelpBox(attribute.comment, (MessageType)attribute.type);
+            if (!attribute.helpButton)
+                gui.HelpBox(attribute.comment, (MessageType)attribute.type);
 		}
+
+        public override void OnLowerGUI()
+        {
+            if (attribute.helpButton)
+            {
+                if (showHelp)
+                    gui.HelpBox(attribute.comment, (MessageType)attribute.type);
+            }
+        }
 	}
 }
--- Assets/VFW Examples/Scripts/Attributes/DecorationsExample.cs
+++ Assets/VFW Examples/Scripts/Attributes/DecorationsExample.cs
@@ -9,5 +9,8 @@ namespace VFWExamples
 
 		[Whitespace(0, Top = 100f), Comment(1, "I'm an int field, and I'm 100 pxs down!")]
 		public int intField;
+
+       [Comment("This is a help text", helpButton:true)]
+        public string StrProperty2 { get; set; }
 	}
 }
