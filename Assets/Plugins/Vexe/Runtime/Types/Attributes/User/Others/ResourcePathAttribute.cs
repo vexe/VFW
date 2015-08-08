e814c50d116b97881cc24cec6e88545b91f87169
james7132 <james.siqi.liu@gmail.com>
2015-08-08 16:56:03 -0400

    Updated ResourcePathAttribute
    Changed the ResourcePathAttribute to show a more intuitive Object field
    instead.
    
    Changed the drawer to use Regex to detect whether the path is a valid
    Resource path. It works much better and with less code than the previous
    solution.
    
    The "Clear Resource" clear button was reading "Clear Clear Resource",
    fixed that.
    
    With that addition, I also added a parameter to the attribute that allows
    a filtering of exactly what kinds of Resources are accepted.
    
    Instead of showing a dubious (missing resource) or (null) output. The
    field displays a seperate debug line underneath the object field.
    
    If a scene object is somehow dragged into the object field (it shouldn't),
    it will say that only Assets are allowed.
    
    If a non-Resource asset is dragged in, it will instead say that is is not
    a Resource, and ask for it to be moved to a Resource Folder.

--- Assets/Plugins/Vexe/Runtime/Types/Attributes/User/Others/ResourcePathAttribute.cs
+++ Assets/Plugins/Vexe/Runtime/Types/Attributes/User/Others/ResourcePathAttribute.cs
@@ -1,17 +1,23 @@
-﻿using UnityEngine;
-using System.Collections;
-using Vexe.Runtime.Types;
-using System;
+﻿using System;
+using UnityObject = UnityEngine.Object;
 
 namespace Vexe.Runtime.Types
 {
 
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
-	public class ResourcePathAttribute : DrawnAttribute
-{
-	public ResourcePathAttribute()
-	{
-	}
-}
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
+	public class ResourcePathAttribute : DrawnAttribute 
+    {
+
+        public Type ResourceType { get; set; }
+
+        public ResourcePathAttribute(Type objectType = null) 
+        {
+	        if (objectType == null || !typeof(UnityObject).IsAssignableFrom(objectType))
+	            ResourceType = typeof(UnityObject);
+	        else
+	            ResourceType = objectType;
+	    }
+
+    }
 
 }
