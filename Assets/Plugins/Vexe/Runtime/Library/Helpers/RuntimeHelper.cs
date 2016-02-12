6d7838f200bf4833a99bb1dc2e7ec35984a6a141
Andrey Popov <andreich.ne@gmail.com>
2016-02-13 00:17:48 +0600

    Double "using" delete

--- Assets/Plugins/Vexe/Runtime/Library/Helpers/RuntimeHelper.cs
+++ Assets/Plugins/Vexe/Runtime/Library/Helpers/RuntimeHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Vexe.Runtime.Extensions;
